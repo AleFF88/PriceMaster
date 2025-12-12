4160d771b690ed0d0d1325fec4fa5e69ace4688f
AleFF88 <1871847@gmail.com>
2025-12-12 16:55:09 +0200

    DbSets created

--- PriceMaster.Infrastructure/PriceMasterDbContext.cs
+++ PriceMaster.Infrastructure/PriceMasterDbContext.cs
@@ -1,6 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CostMaster.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace PriceMaster.Infrastructure {
     internal class PriceMasterDbContext : DbContext {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Series> Series { get; set; }
+        public DbSet<Unit> Units { get; set; }
+        public DbSet<Component> Components { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<BOMItem> BOMItems { get; set; }
+        public DbSet<ProductionHistory> ProductionHistories { get; set; }
+
+        public PriceMasterDbContext(DbContextOptions<PriceMasterDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PriceMasterDbContext).Assembly);
+        }
     }
 }
