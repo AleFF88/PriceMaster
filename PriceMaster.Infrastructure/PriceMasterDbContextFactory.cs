aa21ef9af99c0ced903f91d6d40370ab1d1ed205
AleFF88 <1871847@gmail.com>
2025-12-12 19:53:09 +0200

    Add PriceMasterDbContextFactory to enable EF Core design-time migrations

---
+++ PriceMaster.Infrastructure/PriceMasterDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PriceMaster.Infrastructure {
+    public class PriceMasterDbContextFactory : IDesignTimeDbContextFactory<PriceMasterDbContext> {
+        public PriceMasterDbContext CreateDbContext(string[] args) {
+            var optionsBuilder = new DbContextOptionsBuilder<PriceMasterDbContext>();
+            optionsBuilder.UseSqlite("Data Source=pricemaster.db");
+
+            return new PriceMasterDbContext(optionsBuilder.Options);
+        }
+    }
+}
