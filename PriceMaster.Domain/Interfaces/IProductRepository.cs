0d4123ae39079bf2efde4dd659c2f411288875ac
AleFF88 <1871847@gmail.com>
2025-12-14 22:09:47 +0200

    Add repository for Product

---
+++ PriceMaster.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using PriceMaster.Domain.Entities;
+
+namespace PriceMaster.Domain.Interfaces {
+    public interface IProductRepository {
+        Task Add(Product product); 
+        Task<bool> Exists(string productCode);
+    }
+}
---
+++ PriceMaster.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PriceMaster.Domain.Entities;
+using PriceMaster.Domain.Interfaces;
+
+namespace PriceMaster.Infrastructure.Repositories {
+    public class ProductRepository : IProductRepository {
+        private readonly PriceMasterDbContext _context;
+
+        public ProductRepository(PriceMasterDbContext context) {
+            _context = context;
+        }
+
+        public async Task Add(Product product) {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> Exists(string productCode) {
+            return await _context.Products.AnyAsync(p => p.ProductCode == productCode);
+        }
+    }
+}
