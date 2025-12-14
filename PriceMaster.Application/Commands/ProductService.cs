6ed298cdeb4eae0bb388320f5c3592c22679c7b5
AleFF88 <1871847@gmail.com>
2025-12-14 22:12:58 +0200

    Add a command to add a product

---
+++ PriceMaster.Application/Commands/ProductService.cs
@@ -0,0 +1,16 @@
+﻿using PriceMaster.Domain.Entities;
+using PriceMaster.Domain.Interfaces;
+
+namespace PriceMaster.Application.Commands {
+    public class ProductService {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository) {
+            _productRepository = productRepository;
+        }
+
+        public async Task CreateProduct(Product product) {
+            await _productRepository.Add(product);
+        }
+    }
+}
