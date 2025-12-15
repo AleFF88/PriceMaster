a7111f1ff80bd5d32248fa66f404abe250c927f4
AleFF88 <1871847@gmail.com>
2025-12-15 13:21:20 +0200

    Created DTO for Product

---
+++ PriceMaster.Application/DTOs/ProductResultDto.cs
@@ -0,0 +1,7 @@
+﻿namespace PriceMaster.Application.DTOs {
+    public class ProductResultDto {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string? ProductCode { get; set; }
+    }
+}
