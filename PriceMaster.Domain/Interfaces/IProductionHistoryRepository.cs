3fbfd6067b0036d8e38a64c8bb466808ac95b7b1
AleFF88 <1871847@gmail.com>
2025-12-15 18:59:20 +0200

    Add an interface for the repository for ProductHistory

---
+++ PriceMaster.Domain/Interfaces/IProductionHistoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using PriceMaster.Domain.Entities;
+
+namespace PriceMaster.Domain.Interfaces {
+    public interface IProductionHistoryRepository {
+
+        // <summary>
+        /// Adds a new production history record to the database and saves changes.
+        /// </summary>
+        /// <param name="history">The production history entity to add.</param>
+        Task AddAsync(ProductionHistory history);
+
+        /// <summary>
+        /// Calculates the total value of all manufactured products
+        /// based on their recommended prices stored in production history.
+        /// </summary>
+        /// <returns>The aggregated total value as a decimal.</returns>
+        Task<decimal> GetTotalProductionValueReportAsync(); 
+    }
+}
