781cae560994ee3741eb65afa3b3b2fbe186b4a7
AleFF88 <1871847@gmail.com>
2025-12-15 13:26:37 +0200

    demonstration of the use of business logic to create a new product

--- PriceMaster.ConsoleApp/Program.cs
+++ PriceMaster.ConsoleApp/Program.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PriceMaster.Application.Commands;
+using PriceMaster.Domain.Entities;
 using PriceMaster.Infrastructure;
+using PriceMaster.Infrastructure.Repositories;
 
 namespace PriceMaster.ConsoleApp {
     public static class Program {
-        static void Main() {
+
+        public static async Task Main() {
             var options = new DbContextOptionsBuilder<PriceMasterDbContext>()
                 .UseSqlite("Data Source=pricemaster.db")
                 .Options;
@@ -12,11 +16,26 @@ namespace PriceMaster.ConsoleApp {
                 var pending = context.Database.GetPendingMigrations();
                 if (pending.Any()) {
                     Console.WriteLine($"Apply {pending.Count()} pending migrations.");
-                    context.Database.Migrate(); 
+                    context.Database.Migrate();
                     Console.WriteLine("Database created and migrated successfully.");
                 } else {
                     Console.WriteLine($"No pending migrations.");
                 }
+
+                #region Create Product with ProductCode = "110" for testing purpose
+                var productService = new ProductService(new ProductRepository(context));
+
+                var product = new Product {
+                    ProductCode = "110",
+                    SizeWidth = 0.6m,
+                    SizeHeight = 0.3m,
+                    RecommendedPrice = 2300,
+                    SeriesId = 1
+                };
+
+                var result = await productService.CreateProduct(product);
+                Console.WriteLine(result.Message);
+                #endregion
             }
         }
     }
