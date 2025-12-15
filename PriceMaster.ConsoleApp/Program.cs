b0a9eb648e1644c4429e89421cf9a6d2ce8cb321
AleFF88 <1871847@gmail.com>
2025-12-15 19:59:57 +0200

    Remove redundant 'using'

--- PriceMaster.ConsoleApp/Program.cs
+++ PriceMaster.ConsoleApp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PriceMaster.Application.Commands;
-using PriceMaster.Domain.Entities;
 using PriceMaster.Infrastructure;
 using PriceMaster.Infrastructure.Repositories;
 
