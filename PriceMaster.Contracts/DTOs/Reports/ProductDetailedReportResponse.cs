3a437324a858f745d1de6511ddb94efa8efe066c
AleFF88 <1871847@gmail.com>
2025-12-16 22:07:55 +0200

    Add ProductDetailedReport model in Reports catalog

---
+++ PriceMaster.Contracts/DTOs/Reports/ProductDetailedReportResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace PriceMaster.Contracts.DTOs.Reports {
+    public class ProductDetailedReportResponse {
+        public string ProductCode { get; set; } = string.Empty; 
+        public int Count { get; set; }
+        public decimal TotalValue { get; set; }
+        public decimal WorkCost { get; set; }
+        public DateTime PeriodFrom { get; set; }
+        public DateTime PeriodTo { get; set; }
+    }
+}
