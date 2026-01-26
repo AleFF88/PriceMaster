b8bd196ffe66a13e9e2078c197ec7e685dce0b83
AleFF88 <1871847@gmail.com>
2026-01-26 14:41:17 +0200

    Added DI container

---
+++ PriceMaster.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PriceMaster.Domain.Interfaces;
+using PriceMaster.Infrastructure.Repositories;
+
+namespace PriceMaster.Infrastructure {
+    public static class DependencyInjection {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, Action<DbContextOptionsBuilder> configureDbContext) { 
+            services.AddDbContext<PriceMasterDbContext>(configureDbContext);
+
+            // REPOSITORIES
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductionHistoryRepository, ProductionHistoryRepository>();
+
+            // QUERIES
+            services.AddScoped<IProductionHistoryQueries, ProductionHistoryQueries>();
+
+            // SERVICES
+            //
+
+            return services;
+        }
+    }
+}
