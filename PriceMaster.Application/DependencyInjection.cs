7e9cc500aaf87443b8dfbf8eec3dbb68989db3e4
AleFF88 <1871847@gmail.com>
2026-01-28 20:06:36 +0200

    Product service registered in DI

---
+++ PriceMaster.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PriceMaster.Application.Services;
+
+namespace PriceMaster.Application {
+
+    public static class DependencyInjection {
+        public static IServiceCollection AddApplication(this IServiceCollection services) {
+
+            // SERVICES
+            services.AddScoped<ProductService>();
+
+            return services;
+        }
+    }
+}
--- PriceMaster.ConsoleApp/Program.cs
+++ PriceMaster.ConsoleApp/Program.cs
@@ -1,16 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PriceMaster.Application;
 using PriceMaster.Domain.Interfaces;
 using PriceMaster.Infrastructure;
 
 namespace PriceMaster.ConsoleApp {
     internal static class Program {
-        static void Main(string[] args) {
+        static async Task Main(string[] args) {
             var builder = Host.CreateApplicationBuilder(args);
 
             builder.Services.AddInfrastructure(options =>
                 options.UseSqlite("Data Source=pricemaster.db"));
+            builder.Services.AddApplication();
 
             using IHost host = builder.Build();
 
