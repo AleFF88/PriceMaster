1722e05c9dc0a23160a5de5fe3ded54674fd1273
AleFF88 <1871847@gmail.com>
2026-01-31 21:34:44 +0200

    Added ServiceResult to ProductService

--- PriceMaster.Application/Services/ProductService.cs
+++ PriceMaster.Application/Services/ProductService.cs
@@ -16,12 +16,15 @@ namespace PriceMaster.Application.Services {
         /// Includes storing basic product information and its bill of materials (BOM).
         /// </summary>
         /// <param name="dto">A data transfer object containing product parameters and a list of components.</param>
-        /// <returns>A task representing the asynchronous create operation.</returns>
-
-        public async Task CreateProductAsync(CreateProductDto dto) {
+        /// <returns>Operation result with success flag and message.</returns>
+        public async Task<ServiceResult> CreateProductAsync(CreateProductDto dto) {
             // Check for duplicates (business rule)
-            if (await _productRepository.Exists(dto.ProductCode))
-                throw new InvalidOperationException($"Product with code {dto.ProductCode} already exists.");
+            if (await _productRepository.Exists(dto.ProductCode)) {
+                return new ServiceResult {
+                    IsSuccess = false,
+                    Message = $"Product with code {dto.ProductCode} already exists."
+                };
+            }
 
             // Mapping DTO -> Entity
             var product = new Product {
@@ -38,6 +41,12 @@ namespace PriceMaster.Application.Services {
             };
 
             await _productRepository.AddAsync(product);
+
+
+            return new ServiceResult {
+                IsSuccess = true,
+                Message = string.Empty
+            };
         }
     }
 }
