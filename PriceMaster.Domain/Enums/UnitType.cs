4a63ddc53cada26c27e8cf344408eecc2293a4b8
AleFF88 <1871847@gmail.com>
2026-02-09 21:13:11 +0200

    Change magic numbers to UnitType enum

---
+++ PriceMaster.Domain/Enums/UnitType.cs
@@ -0,0 +1,6 @@
+﻿namespace PriceMaster.Domain.Enums {
+    public enum UnitType {
+        Piece = 1,
+        SquareMeter = 2,
+    }
+}
--- PriceMaster.Infrastructure/Configurations/ComponentConfiguration.cs
+++ PriceMaster.Infrastructure/Configurations/ComponentConfiguration.cs
@@ -23,47 +23,47 @@ namespace PriceMaster.Infrastructure.Configurations {
 
             builder.HasData(
                 // Artifacts, common
-                new Component { ComponentId = 1, ComponentName = "Button", UnitId = 1, PricePerUnit = 5, CategoryId = (int)CategoryType.Artifact },
-                new Component { ComponentId = 2, ComponentName = "Ring", UnitId = 1, PricePerUnit = 5, CategoryId = (int)CategoryType.Artifact },
-                new Component { ComponentId = 3, ComponentName = "Nail", UnitId = 1, PricePerUnit = 5, CategoryId = (int)CategoryType.Artifact },
-                new Component { ComponentId = 4, ComponentName = "Harness Component (simple)", UnitId = 1, PricePerUnit = 10, CategoryId = (int)CategoryType.Artifact },
-                new Component { ComponentId = 5, ComponentName = "Harness Component (stars)", UnitId = 1, PricePerUnit = 20, CategoryId = (int)CategoryType.Artifact },
-                new Component { ComponentId = 6, ComponentName = "Handle", UnitId = 1, PricePerUnit = 50, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 1, ComponentName = "Button", UnitId = (int)UnitType.Piece, PricePerUnit = 5, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 2, ComponentName = "Ring", UnitId = (int)UnitType.Piece, PricePerUnit = 5, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 3, ComponentName = "Nail", UnitId = (int)UnitType.Piece, PricePerUnit = 5, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 4, ComponentName = "Harness Component (simple)", UnitId = (int)UnitType.Piece, PricePerUnit = 10, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 5, ComponentName = "Harness Component (stars)", UnitId = (int)UnitType.Piece, PricePerUnit = 20, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 6, ComponentName = "Handle", UnitId = (int)UnitType.Piece, PricePerUnit = 50, CategoryId = (int)CategoryType.Artifact },
 
                 // Artifacts, coins
-                new Component { ComponentId = 50, ComponentName = "Copper coin (n3)", UnitId = 1, PricePerUnit = 5, CategoryId = (int)CategoryType.Artifact },
-                new Component { ComponentId = 51, ComponentName = "Copper coin (n2)", UnitId = 1, PricePerUnit = 10, CategoryId = (int)CategoryType.Artifact },
-                new Component { ComponentId = 52, ComponentName = "Copper coin (n1)", UnitId = 1, PricePerUnit = 20, CategoryId = (int)CategoryType.Artifact },
-                new Component { ComponentId = 53, ComponentName = "Silver coin (copy)", UnitId = 1, PricePerUnit = 72, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 50, ComponentName = "Copper coin (n3)", UnitId = (int)UnitType.Piece, PricePerUnit = 5, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 51, ComponentName = "Copper coin (n2)", UnitId = (int)UnitType.Piece, PricePerUnit = 10, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 52, ComponentName = "Copper coin (n1)", UnitId = (int)UnitType.Piece, PricePerUnit = 20, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 53, ComponentName = "Silver coin (copy)", UnitId = (int)UnitType.Piece, PricePerUnit = 72, CategoryId = (int)CategoryType.Artifact },
 
                 // Artifacts, balls
-                new Component { ComponentId = 90, ComponentName = "Spherical ball", UnitId = 1, PricePerUnit = 5, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 90, ComponentName = "Spherical ball", UnitId = (int)UnitType.Piece, PricePerUnit = 5, CategoryId = (int)CategoryType.Artifact },
 
                 // Artifacts, chalk copy
-                new Component { ComponentId = 110, ComponentName = "Inkwell", UnitId = 1, PricePerUnit = 30, CategoryId = (int)CategoryType.Artifact },
-                new Component { ComponentId = 111, ComponentName = "Tobacco pipe", UnitId = 1, PricePerUnit = 25, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 110, ComponentName = "Inkwell", UnitId = (int)UnitType.Piece, PricePerUnit = 30, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 111, ComponentName = "Tobacco pipe", UnitId = (int)UnitType.Piece, PricePerUnit = 25, CategoryId = (int)CategoryType.Artifact },
 
                 // Artifacts, ?
-                new Component { ComponentId = 120, ComponentName = "Flint", UnitId = 1, PricePerUnit = 80, CategoryId = (int)CategoryType.Artifact },
+                new Component { ComponentId = 120, ComponentName = "Flint", UnitId = (int)UnitType.Piece, PricePerUnit = 80, CategoryId = (int)CategoryType.Artifact },
 
 
 
                 //Base materials, baguettes
-                new Component { ComponentId = 500, ComponentName = "Baguette 60*30", UnitId = 1, PricePerUnit = 661, CategoryId = (int)CategoryType.BaseMaterial },
+                new Component { ComponentId = 500, ComponentName = "Baguette 60*30", UnitId = (int)UnitType.Piece, PricePerUnit = 661, CategoryId = (int)CategoryType.BaseMaterial },
 
                 //Base materials, printout
-                new Component { ComponentId = 900, ComponentName = "Printout 110", UnitId = 1, PricePerUnit = 71, CategoryId = (int)CategoryType.BaseMaterial },
-                new Component { ComponentId = 901, ComponentName = "Printout 150", UnitId = 1, PricePerUnit = 71, CategoryId = (int)CategoryType.BaseMaterial },
+                new Component { ComponentId = 900, ComponentName = "Printout 110", UnitId = (int)UnitType.Piece, PricePerUnit = 71, CategoryId = (int)CategoryType.BaseMaterial },
+                new Component { ComponentId = 901, ComponentName = "Printout 150", UnitId = (int)UnitType.Piece, PricePerUnit = 71, CategoryId = (int)CategoryType.BaseMaterial },
 
                 //Base materials, other
-                new Component { ComponentId = 950, ComponentName = "Hardware", UnitId = 1, PricePerUnit = 25, CategoryId = (int)CategoryType.BaseMaterial },
-                new Component { ComponentId = 961, ComponentName = "Textile, per sq.m.", UnitId = 2, PricePerUnit = 150, CategoryId = (int)CategoryType.BaseMaterial },
-                new Component { ComponentId = 962, ComponentName = "Polyurethane, per sq.m.", UnitId = 2, PricePerUnit = 85, CategoryId = (int)CategoryType.BaseMaterial },
+                new Component { ComponentId = 950, ComponentName = "Hardware", UnitId = (int)UnitType.Piece, PricePerUnit = 25, CategoryId = (int)CategoryType.BaseMaterial },
+                new Component { ComponentId = 961, ComponentName = "Textile, per sq.m.", UnitId = (int)UnitType.SquareMeter, PricePerUnit = 150, CategoryId = (int)CategoryType.BaseMaterial },
+                new Component { ComponentId = 962, ComponentName = "Polyurethane, per sq.m.", UnitId = (int)UnitType.SquareMeter, PricePerUnit = 85, CategoryId = (int)CategoryType.BaseMaterial },
 
 
 
                 //Assembly work
-                new Component { ComponentId = 1000, ComponentName = "Work", UnitId = 1, PricePerUnit = 800, CategoryId = (int)CategoryType.BaseMaterial }
+                new Component { ComponentId = 1000, ComponentName = "Work", UnitId = (int)UnitType.Piece, PricePerUnit = 800, CategoryId = (int)CategoryType.BaseMaterial }
             );
 
             builder.HasOne(component => component.Category)
